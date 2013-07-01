69f826cc42c6ce374b3499505e124cebac1f47e9
Pavel Dadykin <pavel.splean@gmail.com>
2013-07-01 10:51:25 +0200

    Change hello_world.rb and hello_world_test because of Monkey-patching

--- tests/hello_world_test.rb
+++ tests/hello_world_test.rb
@@ -3,8 +3,12 @@ require "hello_world"
 
 class HelloWorldTest < Minitest::Test
 
+  def setup
+    @test = HelloWorld.new("hello world")
+  end
+
   def test_hello_world
-    assert_equal 'HELLO WORLD', 'hello world'.solution
+    assert_equal 'HELLO WORLD', @test.solution
   end
 
 end
