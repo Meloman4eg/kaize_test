ce7a045d336c30a2f0ce222a98d910c2871c351f
Pavel Dadykin <pavel.splean@gmail.com>
2013-07-01 13:32:40 +0200

    Change test_helper

--- classes/test_helper.rb
+++ classes/test_helper.rb
@@ -1,1 +1,11 @@
 require "minitest/autorun"
+
+# Classes to test
+require "the_truth"
+require "reverse"
+require "maximum"
+require "nil_values"
+require "map"
+require "hello_world"
+require "fizzbuzz"
+require "blackjack"
