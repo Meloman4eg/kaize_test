4ef535228efc312465c555911353a03becea7aab
Pavel Dadykin <pavel.splean@gmail.com>
2013-07-04 12:12:59 +0200

    Update Problem.rb and Problem_test.rb

--- classes/problem.rb
+++ classes/problem.rb
@@ -18,5 +18,33 @@ class Problem
     true
   end
 
+  def home_on_the_range
+    11..94
+  end
+
+  def versus_add
+    "blue"
+  end
+
+  def theres_no_way_this_works_second
+    "HelloWorld"
+  end
+
+  def or_equal
+    "rubeque"
+  end
+
+  def alternate_array_notation(n)
+    case n
+    when 1 then return %w(hello world)
+    when 2 then return %w{1 2 3 4}
+    when 3 then return %w?remembrance of things past?
+    end
+  end
+
+  def array_item_removal
+    
+  end
+
 
 end
--- tests/problem_test.rb
+++ tests/problem_test.rb
@@ -27,4 +27,45 @@ class ProblemTest < Minitest::Test
     assert_equal (str == "My mind is going Dave"), @test.theres_no_way_this_works
   end
 
+  def test_home_on_the_range
+    assert_equal (1..100).to_a[@test.home_on_the_range].reduce(:+), 4494
+  end
+
+  def test_versus_add
+    roses = "blue" && "red"
+    violets = "blue" and "red"
+
+    assert_equal roses, "red"
+    assert_equal violets, @test.versus_add
+  end
+
+  def test_theres_no_way_this_works_second
+    str = "Hello" "World"
+
+    assert_equal str, @test.theres_no_way_this_works_second
+  end
+
+  def test_or_equal
+    b = 8
+    c = false
+
+    a ||= "rubeque"
+    b ||= "rubeque"
+    c ||= "rubeque"
+
+    assert_equal a, "rubeque"
+    assert_equal b, 8
+    assert_equal c, @test.or_equal
+  end
+
+  def test_alternate_array_notation
+    assert_equal @test.alternate_array_notation(1), ["hello", "world"]
+    assert_equal @test.alternate_array_notation(2), ["1", "2", "3", "4"]
+    assert_equal @test.alternate_array_notation(3), ["remembrance", "of", "things", "past"]
+  end
+
+  def test_array_item_removal
+    #assert_equal ([:r, :u, :b, :e, :q, :u, :e] [2,5]), [:b, :q]
+  end
+
 end
